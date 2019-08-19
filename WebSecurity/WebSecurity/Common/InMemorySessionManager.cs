e5068f45c66887ac9b848557ce7f44dd8d24cd02
Raxit Soni <soni_1983@outlook.com>
2019-08-19 23:13:08 +0530

    In memory session manager class added

---
+++ WebSecurity/WebSecurity/Common/InMemorySessionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebSecurity.Models;
+
+namespace WebSecurity.Common
+{
+    public class InMemorySessionManager
+    {
+        private static List<SessionManager> SessionManager { get; set; }
+
+        /// <summary>
+        /// Update session data
+        /// </summary>
+        /// <param name="sessionManager">Session Manager</param>
+        public void UpdateSession(SessionManager sessionManager)
+        {
+
+        }
+    }
+}
