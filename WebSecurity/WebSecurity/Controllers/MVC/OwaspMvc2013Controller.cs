c5d72f486c16a5822a2c7ff301ed0bc686820ecc
Raxit Soni <soni_1983@outlook.com>
2019-08-24 22:37:29 +0530

    Changes for tables.

---
+++ WebSecurity/WebSecurity/Controllers/MVC/OwaspMvc2013Controller.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WebSecurity.Controllers
+{
+    public class OwaspMvc2013Controller : Controller
+    {
+        // GET: OwaspMvc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
