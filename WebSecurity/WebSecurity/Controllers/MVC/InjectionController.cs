e4217c1e495d606fa0232a4a10f59091f343a6dc
Raxit Soni <soni_1983@outlook.com>
2019-09-03 20:19:58 +0530

    XPath injection demo added

--- WebSecurity/WebSecurity/Controllers/MVC/InjectionController.cs
+++ WebSecurity/WebSecurity/Controllers/MVC/InjectionController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Web.Mvc;
+using System.Xml.Linq;
+using System.Xml.XPath;
 using WebSecurity.Models;
 
 namespace WebSecurity.Controllers
@@ -12,6 +16,11 @@ namespace WebSecurity.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        private void SetElementValue(XElement xElement, string value)
+        {
+            xElement.SetElementValue("state", value);
         }
 
 
@@ -40,6 +49,44 @@ namespace WebSecurity.Controllers
             catch(Exception ex)
             {
                 var ex1 = ex;
+            }
+
+            return View("Index");
+        }
+
+
+        // Normal Input: ReportsService, FinancialService
+        // Hacker Input: ' or 1=1 or text()='
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult XPathInjection(FormCollection form)
+        {
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + $"bin/Content/Files/XPathXml.xml";
+            var services = form["xpathQuery"].ToString();
+            var selectedSwitch = form["xpathSwitch"].ToString();
+
+            var servicesArray = services.Split(',');
+
+            foreach (var service in servicesArray)
+            {
+                var document = new XPathDocument(filePath);
+                var navigator = document.CreateNavigator();
+
+                var expression = $@"/services/service/name[text()='{service}']";
+
+                XPathExpression expr = navigator.Compile(expression);
+                XPathNodeIterator iterator = navigator.Select(expr);
+
+                var list = new List<string>();
+
+                var xDoc = XDocument.Load(filePath);
+
+                foreach (XPathNavigator node in iterator)
+                {
+                    var element = xDoc.Descendants("services").Descendants("service").FirstOrDefault(x => x.Element("name").Value == node.Value);
+                    SetElementValue(element, selectedSwitch);
+                    xDoc.Save(filePath);
+                }
             }
 
             return View("Index");
